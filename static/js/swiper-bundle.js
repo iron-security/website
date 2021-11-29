4d5837b9bd50916bf9d5024d98f6a382b15e5ced
Niels Hofmans <niels@ironpeak.be>
2021-11-29 16:25:06 +0100

    Revert "fix: prototype pollution"
    This reverts commit 28b9232224fea08800cdfd0bf4102493e97cc7ce.

--- static/js/swiper-bundle.js
+++ static/js/swiper-bundle.js
@@ -77,10 +77,7 @@
     }
 
     Object.keys(src).forEach(function (key) {
-      if (key === "__proto__" || key === "constructor") continue;
-      
-      if (typeof target[key] === 'undefined') target[key] = src[key];
-      else if (isObject$1(src[key]) && isObject$1(target[key]) && Object.keys(src[key]).length > 0) {
+      if (typeof target[key] === 'undefined') target[key] = src[key];else if (isObject$1(src[key]) && isObject$1(target[key]) && Object.keys(src[key]).length > 0) {
         extend$1(target[key], src[key]);
       }
     });
