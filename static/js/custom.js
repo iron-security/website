3539b48f79081301a8963d3205ce11160014f279
Niels Hofmans <niels@ironpeak.be>
2021-12-04 20:44:08 +0100

    feat: tweakers index ref

--- static/js/custom.js
+++ static/js/custom.js
@@ -51,3 +51,13 @@ function onSubmitCaptcha(captchaToken)
         invalidContactSubmit(err);
     });
 }
+
+function checkTweakers()
+{
+    if (location.pathname !== '/') { return; }
+
+    const urlParams = new URLSearchParams(window.location.search); 
+    if (! urlParams.has('orig') || urlParams.get('orig') !== 'tweakers') { return; }
+
+    $('#txtSubtext').text('like a cyber Tweaker.');
+}; checkTweakers();
